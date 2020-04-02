bdc7be5817b629920f931b844f66f0f4567de7bf
Kc Kai <kirstenckai@gmail.com>
2020-04-02 23:27:07 +0000

    Update to use parameter queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,20 @@ const{ Pool } = require('pg');
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${[process.argv[2]]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher
-`)
+`
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`)
